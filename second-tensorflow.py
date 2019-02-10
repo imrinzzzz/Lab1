6d49b5e90e203a14ea9980bfccffc424d1028d89
Thanirin <thanirin.tri@student.mahidol.ac.th>
2019-02-10 16:11:03 +0700

    second-tensorflow: tensorflow can't iterate

--- second-tensorflow.py
+++ second-tensorflow.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 # # constant 1D tensor (vector)
@@ -71,16 +72,36 @@ import tensorflow as tf
 
 # ====================== range ======================
 
-# start = 3, limit = 18, delta = 3
-a = tf.range(3, 18, 3)
+# # start = 3, limit = 18, delta = 3
+# a = tf.range(3, 18, 3)
+#
+# b = tf.range(3, 1, -0.5)
+#
+# # limit = 5
+# c = tf.range(5)
+#
+# with tf.Session() as session:
+#     writer = tf.summary.FileWriter('./graphs', session.graph)
+#     print(session.run(a), session.run(b), session.run(c))
+#
+# writer.close()
 
-b = tf.range(3, 1, -0.5)
+# ______________________________________________________________
 
-# limit = 5
-c = tf.range(5)
+# ====================== zeros ======================
 
-with tf.Session() as session:
-    writer = tf.summary.FileWriter('./graphs', session.graph)
-    print(session.run(a), session.run(b), session.run(c))
+# unlike numpy or py sequence, TensorFlow sequences are not iterable
 
-writer.close()
+# for x in np.linspace(0, 10, 5):
+#     print(x)
+
+# with tf.Session() as session:
+#     writer = tf.summary.FileWriter('./graphs', session.graph)
+#     for x in tf.linspace(0, 10, 4):
+#         print(session.run(x))
+#
+# writer.close()
+
+# ______________________________________________________________
+
+# ====================== random constants ======================
