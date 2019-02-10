962be6e7d9cf69144b2336754e1a67d0d0e91282
Thanirin <thanirin.tri@student.mahidol.ac.th>
2019-02-10 15:41:24 +0700

    second-tensorflow: add zeros and ones

--- second-tensorflow.py
+++ second-tensorflow.py
@@ -1,15 +1,36 @@
 import tensorflow as tf
 
-# constant 1D tensor (vector)
-a = tf.constant([2, 2], name="vector")
+# # constant 1D tensor (vector)
+# a = tf.constant([2, 2], name="vector")
+#
+# # constant 2x2 tensor (matrix)
+# b = tf.constant([[0, 1], [2, 3]], name="b")
+#
+# # create a tensor of shape and all elements are 0s
+# a = tf.zeros([2, 3], tf.int32)
+#
+# with tf.Session as session:
+#     writer = tf.summary.FileWriter('./graphs', session.graph)
+#     print(session.run(a))
+#
+# writer.close()
 
-# constant 2x2 tensor (matrix)
-b = tf.constant([[0, 1], [2, 3]], name="b")
+# _____________________________________________________________
 
-# create a tensor of shape and all elements are 0s
-a = tf.zeros([2, 3], tf.int32)
+# input_tensor = [[0, 1], [2, 3], [4, 5]]
+# a = tf.zeros_like(input_tensor, name="input_tensor")
+#
+# with tf.Session() as session:
+#     writer = tf.summary.FileWriter('./graphs', session.graph)
+#     print(session.run(a))
+#
+# writer.close()
 
-with tf.Session as session:
+# _____________________________________________________________
+
+a = tf.ones([2, 3], tf.int32)
+
+with tf.Session() as session:
     writer = tf.summary.FileWriter('./graphs', session.graph)
     print(session.run(a))
 
