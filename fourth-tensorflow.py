0841aa472b93d7f1fde30c490b2c31aac7e28bc6
Thanirin <thanirin.tri@student.mahidol.ac.th>
2019-02-10 16:48:45 +0700

    second-tensorflow: change comment type fourth-tensorflow: add tf.ones([2,2])...

--- fourth-tensorflow.py
+++ fourth-tensorflow.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
+import numpy as np
 
 # Python native types
 
 t_0 = 19
 t_1 = [b"apple", b"peach", b"grape"]
-
 t_2 = [[True, False, False],
        [False, False, False],
        [False, True, False]]
@@ -15,6 +15,7 @@ with tf.Session() as session:
 
     print(session.run(tf.zeros_like(t_1)))
     # print(session.run(tf.ones_like(t_1))) # expect string, got 1 (int) instead
+    print(session.run(tf.ones([2, 2], np.float32))) # or tf.float32 is Ok too
 
     print(session.run(tf.zeros_like(t_2)))
     print(session.run(tf.ones_like(t_2)))
--- second-tensorflow.py
+++ second-tensorflow.py
@@ -1,90 +1,102 @@
 import numpy as np
 import tensorflow as tf
 
-# # constant 1D tensor (vector)
-# a = tf.constant([2, 2], name="vector")
-#
-# # constant 2x2 tensor (matrix)
-# b = tf.constant([[0, 1], [2, 3]], name="b")
-#
-# # create a tensor of shape and all elements are 0s
-# a = tf.zeros([2, 3], tf.int32)
-#
-# with tf.Session as session:
-#     writer = tf.summary.FileWriter('./graphs', session.graph)
-#     print(session.run(a))
-#
-# writer.close()
+'''
+# constant 1D tensor (vector)
+a = tf.constant([2, 2], name="vector")
+
+# constant 2x2 tensor (matrix)
+b = tf.constant([[0, 1], [2, 3]], name="b")
+
+# create a tensor of shape and all elements are 0s
+a = tf.zeros([2, 3], tf.int32)
+
+with tf.Session as session:
+    writer = tf.summary.FileWriter('./graphs', session.graph)
+    print(session.run(a))
+
+writer.close()
+'''
 
 # _____________________________________________________________
 
 # ====================== zeros ======================
 
-# input_tensor = [[0, 1], [2, 3], [4, 5]]
-# a = tf.zeros_like(input_tensor, name="input_tensor")
-#
-# with tf.Session() as session:
-#     writer = tf.summary.FileWriter('./graphs', session.graph)
-#     print(session.run(a))
-#
-# writer.close()
+"""
+input_tensor = [[0, 1], [2, 3], [4, 5]]
+a = tf.zeros_like(input_tensor, name="input_tensor")
+
+with tf.Session() as session:
+    writer = tf.summary.FileWriter('./graphs', session.graph)
+    print(session.run(a))
+
+writer.close()
+"""
 
 # _____________________________________________________________
 
 # ====================== ones ======================
 
-# a = tf.ones([2, 3], tf.int32)
-# input_tensor = [[0, 1], [2, 3]]
-# b = tf.ones_like(input_tensor)
-#
-# with tf.Session() as session:
-#     writer = tf.summary.FileWriter('./graphs', session.graph)
-#     print(session.run(a))
-#     print(session.run(b))
-#
-# writer.close()
+'''
+a = tf.ones([2, 3], tf.int32)
+input_tensor = [[0, 1], [2, 3]]
+b = tf.ones_like(input_tensor)
+
+with tf.Session() as session:
+    writer = tf.summary.FileWriter('./graphs', session.graph)
+    print(session.run(a))
+    print(session.run(b))
+
+writer.close()
+'''
 
 # _____________________________________________________________
 
 # ====================== fill ======================
 
-# a = tf.fill([2,3], 8)
-#
-# with tf.Session() as session:
-#     writer = tf.summary.FileWriter('./graphs', session.graph)
-#     print(session.run(a))
-#
-# writer.close()
+'''
+a = tf.fill([2,3], 8)
+
+with tf.Session() as session:
+    writer = tf.summary.FileWriter('./graphs', session.graph)
+    print(session.run(a))
+
+writer.close()
+'''
 
 # ______________________________________________________________
 
 # ====================== linespace ======================
 
-# a = tf.linspace(10.0, 13.0, 4, name="linespace")
-#
-# with tf.Session() as session:
-#     writer = tf.summary.FileWriter('./graphs', session.graph)
-#     print(session.run(a))
-#
-# writer.close()
+"""
+a = tf.linspace(10.0, 13.0, 4, name="linespace")
+
+with tf.Session() as session:
+    writer = tf.summary.FileWriter('./graphs', session.graph)
+    print(session.run(a))
+
+writer.close()
+"""
 
 # ______________________________________________________________
 
 # ====================== range ======================
 
-# # start = 3, limit = 18, delta = 3
-# a = tf.range(3, 18, 3)
-#
-# b = tf.range(3, 1, -0.5)
-#
-# # limit = 5
-# c = tf.range(5)
-#
-# with tf.Session() as session:
-#     writer = tf.summary.FileWriter('./graphs', session.graph)
-#     print(session.run(a), session.run(b), session.run(c))
-#
-# writer.close()
+'''
+# start = 3, limit = 18, delta = 3
+a = tf.range(3, 18, 3)
+
+b = tf.range(3, 1, -0.5)
+
+# limit = 5
+c = tf.range(5)
+
+with tf.Session() as session:
+    writer = tf.summary.FileWriter('./graphs', session.graph)
+    print(session.run(a), session.run(b), session.run(c))
+
+writer.close()
+'''
 
 # ______________________________________________________________
 
@@ -92,15 +104,17 @@ import tensorflow as tf
 
 # unlike numpy or py sequence, TensorFlow sequences are not iterable
 
-# for x in np.linspace(0, 10, 5):
-#     print(x)
+'''
+for x in np.linspace(0, 10, 5):
+    print(x)
 
-# with tf.Session() as session:
-#     writer = tf.summary.FileWriter('./graphs', session.graph)
-#     for x in tf.linspace(0, 10, 4):
-#         print(session.run(x))
-#
-# writer.close()
+with tf.Session() as session:
+    writer = tf.summary.FileWriter('./graphs', session.graph)
+    for x in tf.linspace(0, 10, 4):
+        print(session.run(x))
+
+writer.close()
+'''
 
 # ______________________________________________________________
 
