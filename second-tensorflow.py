335b91efe2548718dfdeb8b61a96eb8d1338c16e
Thanirin <thanirin.tri@student.mahidol.ac.th>
2019-02-10 16:05:58 +0700

    second-tensorflow: add some other more stuffs

--- second-tensorflow.py
+++ second-tensorflow.py
@@ -17,6 +17,8 @@ import tensorflow as tf
 
 # _____________________________________________________________
 
+# ====================== zeros ======================
+
 # input_tensor = [[0, 1], [2, 3], [4, 5]]
 # a = tf.zeros_like(input_tensor, name="input_tensor")
 #
@@ -28,10 +30,57 @@ import tensorflow as tf
 
 # _____________________________________________________________
 
-a = tf.ones([2, 3], tf.int32)
+# ====================== ones ======================
+
+# a = tf.ones([2, 3], tf.int32)
+# input_tensor = [[0, 1], [2, 3]]
+# b = tf.ones_like(input_tensor)
+#
+# with tf.Session() as session:
+#     writer = tf.summary.FileWriter('./graphs', session.graph)
+#     print(session.run(a))
+#     print(session.run(b))
+#
+# writer.close()
+
+# _____________________________________________________________
+
+# ====================== fill ======================
+
+# a = tf.fill([2,3], 8)
+#
+# with tf.Session() as session:
+#     writer = tf.summary.FileWriter('./graphs', session.graph)
+#     print(session.run(a))
+#
+# writer.close()
+
+# ______________________________________________________________
+
+# ====================== linespace ======================
+
+# a = tf.linspace(10.0, 13.0, 4, name="linespace")
+#
+# with tf.Session() as session:
+#     writer = tf.summary.FileWriter('./graphs', session.graph)
+#     print(session.run(a))
+#
+# writer.close()
+
+# ______________________________________________________________
+
+# ====================== range ======================
+
+# start = 3, limit = 18, delta = 3
+a = tf.range(3, 18, 3)
+
+b = tf.range(3, 1, -0.5)
+
+# limit = 5
+c = tf.range(5)
 
 with tf.Session() as session:
     writer = tf.summary.FileWriter('./graphs', session.graph)
-    print(session.run(a))
+    print(session.run(a), session.run(b), session.run(c))
 
 writer.close()
